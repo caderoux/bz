f835c033a71de59faa48dc467447795848c64799
caderoux <cade@roux.org>
2021-01-02 23:05:21 -0600

    Clean up bzcli

--- bzcli/get-busystatus.js
+++ bzcli/get-busystatus.js
@@ -1,16 +1,10 @@
 'use strict';
 
-var badges = [];
-
 const { Command } = require('commander');
-const program = new Command();
-
-program
-.option('-h, --host <hostname>', 'Server host name', 'localhost')
-.option('-p, --port <port number>', 'Server TCP port', '8080')
-;
-
-program.parse(process.argv);
+const program = new Command()
+  .option('-h, --host <hostname>', 'Server host name', 'localhost')
+  .option('-p, --port <port number>', 'Server TCP port', '8080')
+  .parse(process.argv);
 
 const http = require('http');
 
--- bzcli/set-busystatus.js
+++ bzcli/set-busystatus.js
@@ -1,22 +1,19 @@
 'use strict';
 
-var badges = [];
-
+const os = require("os");
+const ip = require("ip");
 const { Command } = require('commander');
-const program = new Command();
-
-program
-.option('-h, --host <hostname>', 'Server host name', 'localhost')
-.option('-p, --port <port number>', 'Server TCP port', '8080')
-.option('-id, --badgeid <url>', 'Badge identifier', 'http://localhost')
-.option('-s, --status <status text>', 'Status text', 'Busy')
+const program = new Command()
+  .option('-h, --host <hostname>', 'Server host name', 'localhost')
+  .option('-p, --port <port number>', 'Server TCP port', '8080')
+  .option('-id, --badgeid <url>', 'Badge identifier', `${ip.address()}(${os.hostname()})`)
+  .option('-s, --status <status text>', 'Status text', 'Busy')
+  .parse(process.argv)
 ;
-
-program.parse(process.argv);
 
 const http = require('http');
 
-let body = JSON.stringify({ badgeId : program.badgeid, statusName : program.status });
+let body = JSON.stringify([{ badgeId : program.badgeid, statusName : program.status }]);
   
 let options = {
     hostname: program.host,

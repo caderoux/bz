d266413ba6361a5cea96c9066279f6ea7fbcf108
caderoux <cade@roux.org>
2021-01-02 19:16:46 -0600

    Restructure

--- bzin/bzin.py
+++ bzin/bzin.py
@@ -1,66 +1,77 @@
 #!/usr/bin/env python3
 
 import argparse
-from gpiozero import Button
-import sys
-import requests
-from signal import pause
 import platform
 import socket
+import requests
+from gpiozero import Button
+import sys
+from signal import pause
 
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.255.255.255', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
+class bzin:
+    @staticmethod
+    def SetupParams():
+        def get_ip():
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            try:
+                # doesn't even have to be reachable
+                s.connect(('10.255.255.255', 1))
+                IP = s.getsockname()[0]
+            except Exception:
+                IP = '127.0.0.1'
+            finally:
+                s.close()
+            return IP
 
-parser = argparse.ArgumentParser(description = 'bzin - input hardware controller')
-parser.add_argument('--badgeid', dest = 'badgeid', default = f"{get_ip()}({platform.node()})")
-parser.add_argument('--device', dest = 'device', default = f"UnicornHatMini")
-parser.add_argument('--server', dest = 'server', default = 'localhost')
-parser.add_argument('--port', dest = 'port', default = '8080')
-parser.add_argument('--method', dest = 'method', default = '/api/input')
-args = parser.parse_args()
-serviceUrl = f"http://{args.server}:{args.port}{args.method}"
+        parser = argparse.ArgumentParser(description = 'bzin - input hardware controller')
+        parser.add_argument('--badgeid', dest = 'badgeid', default = f"{get_ip()}({platform.node()})")
+        parser.add_argument('--device', dest = 'device', default = f"UnicornHatMini")
+        parser.add_argument('--server', dest = 'server', default = 'localhost')
+        parser.add_argument('--port', dest = 'port', default = '8080')
+        parser.add_argument('--method', dest = 'method', default = '/api/input')
+        return parser.parse_args()
 
-print(f"""bzin.py: POSTing button messages to {serviceUrl}, press Ctrl+C to exit""")
+    class bzbutton:
+        def button_pressed(self, button):
+            json = {'input' : {
+                                'badgeid' : self.parent.args.badgeid
+                                , 'device' : self.parent.args.device
+                                , 'button' : self.name
+                                }
+                    }
+            print(f"[BZin] Button {self.name} pressed")
+            print(f"[BZin] OUT:POST {self.parent.serviceUrl}\n{json}")
+            r = requests.post(url = self.parent.serviceUrl, json = json)
+            print(f"[BZin] OUT:POST RESPONSE {r.status_code} {r.reason}")
 
-button_map = {
-    5: { 'name' : "A" }
-    , 6: { 'name' : "B" }
-    , 16: { 'name' : "X" }
-    , 24: { 'name' : "Y" }
-}
+        def __init__(self, parent, pin, name):
+            self.parent = parent
+            self.name = name
+            self.button = Button(pin)
+            self.button.when_pressed = self.button_pressed.__get__(self, self.__class__)
 
-def init():
-    for pin, obj in button_map.items():
-        obj['pin'] = pin
-        obj['button'] = Button(pin)
-        obj['button'].when_pressed = button_pressed
+        def close(self):
+            self.button.close()
 
-def finished():
-    for button in button_map.values():
-        button['button'].close()
-    sys.exit(0)
+    def __init__(self):
+        self.args = bzin.SetupParams()
+        self.serviceUrl = f"http://{self.args.server}:{self.args.port}{self.args.method}"
+        self.buttons = [
+            self.bzbutton(self, 5, "A")
+            , self.bzbutton(self, 6, "B")
+            , self.bzbutton(self, 16, "X")
+            , self.bzbutton(self, 24, "Y")
+        ]
+        print(f"""bzin.py: POSTing button messages to {self.serviceUrl}""")
 
-def button_pressed(button):
-    button_name = button_map[button.pin.number]['name']
-    json = {'input' : {'badgeid' : args.badgeid, 'device' : args.device, 'button' : button_name}}
-    print(f"[BZin] Button {button_name} pressed")
-    print(f"[BZin] OUT:POST {serviceUrl} {json}")
-    r = requests.post(url = serviceUrl, json = json)
-    print(r.status_code, r.reason)
+    def finished(self):
+        for button in self.buttons:
+            button.close()
 
 try:
-    init()
-
+    bz = bzin()
     pause()
 
 except KeyboardInterrupt:
-    finished()
+    bz.finished()
+    sys.exit(0)

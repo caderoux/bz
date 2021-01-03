28657d86728b3643fb3fa366d625b83c241013d2
caderoux <cade@roux.org>
2021-01-02 23:05:53 -0600

    Change parameter defaults to ipaddress(name) default convention

--- bzcmd/Set-BzStatus.ps1
+++ bzcmd/Set-BzStatus.ps1
@@ -1,13 +1,14 @@
 param (
     [string] $Server = "localhost"
     , [string] $Port = "8080"
-    , [string] $badgeId = "http://localhost"
+    , [string] $badgeId = "$((Get-NetIPAddress | Where-Object {$_.AddressState -eq "Preferred" -and $_.ValidLifetime -lt "24:00:00"}).IPAddress)($($env:COMPUTERNAME))"
     , [string] $statusName = "Busy"
 )
 
 (
-    Invoke-WebRequest -Uri "http://$Server`:$Port/api/badges" -Method POST -Body (@{ "badgeId" = $badgeId; "statusName" = $statusName } |
-    ConvertTo-Json
-) -ContentType "application/json").Content |
+    Invoke-WebRequest -Uri "http://$Server`:$Port/api/badges" -Method POST -Body (
+            [@{ "badgeId" = $badgeId; "statusName" = $statusName }] | ConvertTo-Json
+        ) -ContentType "application/json"
+).Content |
     ConvertFrom-Json |
     Format-Table -AutoSize -Property badgeId, statusName
